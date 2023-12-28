457a82b4e764d6bc67172de0ea9e4698272ca1e5
rx <132195314+victorxph@users.noreply.github.com>
2023-12-28 18:55:07 -0300

    1.0.0

---
+++ package/index.js
@@ -0,0 +1,29 @@
+class pubSub {
+	constructor() {
+		this.events = {}
+	}
+
+	sub(eventName, fn) {
+		this.events[eventName] = this.events[eventName] || [];
+		this.events[eventName].push(fn);
+	}
+
+	unSub(eventName, fn) {
+		if (this.events[eventName]) {
+			for (i = 0; i < this.events[eventName].length; i++) {
+				if (this.events[eventName][i] === fn) {
+					this.events[eventName].splice(i, 1);
+					break;
+				}
+			}
+		}
+	}
+
+	pub(eventName, data) {
+		if (this.events[eventName]) {
+			this.events[eventName].forEach(fn => {
+				fn(data);
+			})
+		}
+	}
+}
